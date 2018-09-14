a67ac59032d431033f6041fd7064dc3a0e29bea2
胡丕坏 <hupihuai@makeblock.com>
2018-09-14 10:52:29 +0800

    适配到子view

--- app/src/main/java/com/weihu/autolayout/AdapterConstraintLayout.java
+++ app/src/main/java/com/weihu/autolayout/AdapterConstraintLayout.java
@@ -11,6 +11,9 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.content.Context.WINDOW_SERVICE;
 
@@ -21,7 +24,8 @@ import static android.content.Context.WINDOW_SERVICE;
 public class AdapterConstraintLayout extends ConstraintLayout {
     private static final int DESIGN_WIDTH = 1080;
     private static final int DESIGN_HEIGHT = 1920;
-    private static final float DESIGN_SCALE = 3.0f;
+    private static final float DESIGN_SCALE = 3f;
+
     private float mScale;
     private float mFontScale;
     private float mScaleX = 0;
@@ -54,32 +58,130 @@ public class AdapterConstraintLayout extends ConstraintLayout {
         mScaleX *= density;
         mScaleY *= density;
         mFontScale = minScale * scaleDensity;
+        transformSize(this, getLayoutParams());
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+    }
+
+    @Override
+    public void setLayoutParams(ViewGroup.LayoutParams params) {
+        super.setLayoutParams(params);
+        transformSize(this, params);
     }
 
     @Override
     public void addView(View child) {
         super.addView(child);
+        if (!isInEditMode()) {
+            adapterChild(child, child.getLayoutParams());
+        }
     }
 
     @Override
     public void addView(View child, int index) {
         super.addView(child, index);
+        if (!isInEditMode()) {
+            adapterChild(child, child.getLayoutParams());
+        }
     }
 
     @Override
     public void addView(View child, int width, int height) {
         super.addView(child, width, height);
+        if (!isInEditMode()) {
+            adapterChild(child, child.getLayoutParams());
+        }
     }
 
     @Override
     public void addView(View child, ViewGroup.LayoutParams params) {
         if (!isInEditMode()) {
-            transformSize(child, (LayoutParams) params);
+            adapterChild(child, params);
         }
         super.addView(child, params);
     }
 
-    private void transformSize(View child, LayoutParams params) {
+    @Override
+    public void onViewRemoved(View view) {
+        super.onViewRemoved(view);
+        view.setTag(R.id.has_adapter_size, null);
+    }
+
+    private void adapterChild(View view, ViewGroup.LayoutParams params) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            int childCount = viewGroup.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View childView = viewGroup.getChildAt(i);
+                adapterChild(childView, childView.getLayoutParams());
+            }
+        } else {
+            transformSize(view, params);
+        }
+    }
+
+    private void transformSize(View view, ViewGroup.LayoutParams params) {
+        Object tag = view.getTag(R.id.has_adapter_size);
+        if (tag != null || params == null) {//处理过就不处理了
+            return;
+        }
+        view.setTag(R.id.has_adapter_size, true);
+        adapterSize(params);
+        adapterTextSize(view);
+        adapterMargin(params);
+        adapterPadding(view);
+        adapterMinimumSize(view);
+    }
+
+    private void adapterMinimumSize(View view) {
+        if (view.getMinimumWidth() > 0) {
+            view.setMinimumWidth((int) (view.getMinimumWidth() * mScaleX));
+        }
+        if (view.getMinimumHeight() > 0) {
+            view.setMinimumHeight((int) (view.getMinimumHeight() * mScaleY));
+        }
+
+    }
+
+    private void adapterPadding(View view) {
+        //padding
+        int paddingLeft = (int) (view.getPaddingLeft() * mScaleX);
+        int paddingTop = (int) (view.getPaddingTop() * mScaleY);
+        int paddingRight = (int) (view.getPaddingRight() * mScaleX);
+        int paddingBottom = (int) (view.getPaddingBottom() * mScaleY);
+        view.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+    }
+
+    private void adapterMargin(ViewGroup.LayoutParams params) {
+        if (params instanceof MarginLayoutParams) {
+            MarginLayoutParams marginLayoutParams = (MarginLayoutParams) params;
+            //margin
+            marginLayoutParams.leftMargin *= mScaleX;
+            marginLayoutParams.topMargin *= mScaleY;
+            marginLayoutParams.rightMargin *= mScaleX;
+            marginLayoutParams.bottomMargin *= mScaleY;
+        }
+    }
+
+    private void adapterTextSize(View view) {
+        //font size
+        if (view instanceof TextView) {
+            final float textSize = ((TextView) view).getTextSize();
+            ((TextView) view).setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * mFontScale);
+        } else if (view instanceof Button) {
+            final float textSize = ((Button) view).getTextSize();
+            ((Button) view).setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * mFontScale);
+        } else if (view instanceof EditText) {
+            final float textSize = ((EditText) view).getTextSize();
+            ((EditText) view).setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * mFontScale);
+        }
+    }
+
+    private void adapterSize(ViewGroup.LayoutParams params) {
         if (params.width > 0 && params.height > 0) {//按比列
             params.width *= mScale;
             params.height *= mScale;
@@ -93,29 +195,6 @@ public class AdapterConstraintLayout extends ConstraintLayout {
                 params.height *= mScaleY;
             }
         }
-
-        //font size
-        if (child instanceof AppCompatTextView) {
-            final float textSize = ((AppCompatTextView) child).getTextSize();
-            ((AppCompatTextView) child).setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * mFontScale);
-        } else if (child instanceof AppCompatButton) {
-            final float textSize = ((AppCompatButton) child).getTextSize();
-            ((AppCompatButton) child).setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * mFontScale);
-        } else if (child instanceof AppCompatEditText) {
-            final float textSize = ((AppCompatEditText) child).getTextSize();
-            ((AppCompatEditText) child).setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * mFontScale);
-        }
-        //margin
-        params.leftMargin *= mScaleX;
-        params.topMargin *= mScaleY;
-        params.rightMargin *= mScaleX;
-        params.bottomMargin *= mScaleY;
-        //padding
-        int paddingLeft = (int) (getPaddingLeft() * mScaleX);
-        int paddingTop = (int) (getPaddingTop() * mScaleY);
-        int paddingRight = (int) (getPaddingRight() * mScaleX);
-        int paddingBottom = (int) (getPaddingBottom() * mScaleY);
-        child.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
     }
 
 }
